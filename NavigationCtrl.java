12b8f24f1361e61dbbf4f8b849457afa147b4f05
jo-neill <jjo5151@psu.edu>
2016-11-10 18:58:51 -0500

    Delete NavigationCtrl.java

--- NavigationCtrl.java
+++
@@ -1,9 +0,0 @@
-package credentialmanager;
-
-//Cotrols the navigation of the program
-public class NavigationCtrl {
-    public NavigationCtrl() {
-        // App is set to terminate for now but will eventually navigate users to somewhere else
-        System.exit(0);
-    }
-}
