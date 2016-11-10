efeda7efd9619b235b8469638fb1dfaaf488eb73
jo-neill <jjo5151@psu.edu>
2016-11-10 18:57:53 -0500

    Delete CredentialManager.java

--- CredentialManager.java
+++
@@ -1,9 +0,0 @@
-package credentialmanager;
-
-//Begins the program by creating the main menu
-public class CredentialManager {
-
-    public static void main(String[] args) {
-        new AppMenu();
-    }
-}
