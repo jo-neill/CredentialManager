e4be697f9879781ba05b3c189d04e01448e2aa59
npl5035 <npl5035@psu.edu>
2016-12-09 01:40:55 -0500

    Add files via upload

--- MainMenu.java
+++ MainMenu.java
@@ -4,6 +4,9 @@ import java.awt.FlowLayout;
 import java.awt.GridLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.io.IOException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
@@ -18,38 +21,43 @@ public class MainMenu extends JFrame implements ActionListener{
     private JPanel panel3;
     private JLabel usernameLabel;
     private JLabel passwordLabel;
+    private JLabel label;
     private JTextField usernameField;
     private JPasswordField passwordField;
     private JButton btnGenerate;
     private JButton btnClose;
     private JButton btnAddAccount;
-    
-    public static void main(String[] args){
-        new MainMenu();
-    }
+    private JButton btnView;
     
     public MainMenu(){
-        frame = new JFrame("Choose a feature");
-        frame.setLayout(new GridLayout(3,1));
+        frame = new JFrame("Credential Manager");
+        frame.setLayout(new GridLayout(4,1));
         panel = new JPanel(new FlowLayout());
         panel2 = new JPanel(new FlowLayout());
         panel3 = new JPanel(new FlowLayout());
         
+        label = new JLabel("Please choose a feature.");
+        
         btnGenerate = new JButton("Generate");
         btnGenerate.addActionListener(this);
-        btnClose = new JButton("Close");
-        btnClose.addActionListener(this);
         btnAddAccount = new JButton("Add Account");
         btnAddAccount.addActionListener(this);
+        btnClose = new JButton("Close");
+        btnClose.addActionListener(this);
+        btnView = new JButton("View Accounts");
+        btnView.addActionListener(this);
         
-        panel2.add(btnGenerate);
-        panel2.add(btnClose);
-        panel2.add(btnAddAccount);
         
-        frame.add(panel);
+        panel.add(label);
+        panel3.add(btnGenerate);
+        panel3.add(btnView);
+        panel3.add(btnAddAccount);
+        panel3.add(btnClose);
+        
         frame.add(panel2);
+        frame.add(panel);
         frame.add(panel3);
-        frame.setSize(350, 200);
+        frame.setSize(300, 280);
         frame.setResizable(false);
         frame.setLocationRelativeTo(null);
         frame.setDefaultCloseOperation(EXIT_ON_CLOSE);
@@ -68,11 +76,19 @@ public class MainMenu extends JFrame implements ActionListener{
             this.frame.setVisible(false);
         }
         else if(arg.equals("Add Account")){
-            AccountCtrl acctCtrl= new AccountCtrl();
+            try {
+                AccountCtrl acctCtrl= new AccountCtrl();
+            } catch (IOException ex) {
+                Logger.getLogger(MainMenu.class.getName()).log(Level.SEVERE, null, ex);
+            }
             this.frame.setVisible(false);
         }
         else if(arg.equals("Close")){
             System.exit(0);
+        }
+        else if(arg.equals("View Accounts")){
+            UserAccountView acounts = new UserAccountView();
+            this.frame.setVisible(false);
         }
     }
 }
